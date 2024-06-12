801e41db45700b54793dc8d374effbee74a40abc
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-06-12 12:46:39 -0700

    Layout, Nav, Footer, and Route initialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { memo } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Layout } from './components/Layout';
 
+const MemoizedLayout = memo(Layout)
 
 function App() {
 
   return (
     <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MemoizedLayout />} />
+        </Routes>
+      </Router>
     </>
   )
 }
---
+++ src/components/Layout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { Nav } from "./Nav";
+
+export const Layout = () => {
+    return(
+        <>
+            <Nav />
+            <main>
+                <Outlet />
+            </main>
+            <Footer />
+        </>
+    );
+}
