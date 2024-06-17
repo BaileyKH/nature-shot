40ccae50de5d67b8c215f0d13d02b61322a4f684
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-06-17 15:03:40 -0700

    contact form | country default

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -134,6 +134,7 @@ export const Contact = () => {
                                             autoComplete="country-name"
                                             className="block w-full rounded-md border-0 py-1.5 text-white/90 shadow-sm ring-1 ring-inset ring-white/60 bg-white/10 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:max-w-xs sm:text-sm sm:leading-6"
                                         >
+                                            <option selected>-select country-</option>
                                             <option>United States</option>
                                             <option>Finland</option>
                                             <option>New Zealand</option>
